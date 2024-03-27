bde5faa71ff3210d7f3e84eae2ff06ed0febda66
Ankush Goel <goelankush1312@gmail.com>
2024-03-27 16:24:32 +0530

    Dynamic data from Supabase

---
+++ keys.js
@@ -0,0 +1,3 @@
+// Supabase keys
+export const apiKey = "Enter your API key from supabase here"
+export const authorizationToken = "Enter your Authorization Bearer Token from supabase here"
--- script.js
+++ script.js
@@ -11,55 +11,10 @@ btn.addEventListener("click", function() {
     }
 })
 
+
+import { CATEGORIES, initialFacts } from './data.js';
+
 const factsList = document.querySelector(".facts-list");
-
-const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
-];
-
-// console.log(CATEGORIES.find((cat) => cat.name == "society"));
-
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    up_votes: 24,
-    mindblowing_votes: 9,
-    down_votes: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    up_votes: 11,
-    mindblowing_votes: 2,
-    down_votes: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    up_votes: 8,
-    mindblowing_votes: 3,
-    down_votes: 1,
-    createdIn: 2015,
-  },
-];
-
 factsList.innerHTML = "";
 createFactsList(initialFacts)
 
@@ -80,3 +35,18 @@ function createFactsList(dataArray) {
     )
     factsList.insertAdjacentHTML("afterbegin", htmlArr.join(" "))
 }
+
+//Load data from Supabase
+import { apiKey, authorizationToken } from './keys.js';
+async function fetchFacts() {
+    const res = await fetch('https://rjaldysjftmvsvkmipaz.supabase.co/rest/v1/facts', {
+        headers: {
+            apikey: apiKey ,
+            authorization: authorizationToken
+        }       
+    })
+    const data = await res.json()
+    createFactsList(data)
+}
+
+fetchFacts();
